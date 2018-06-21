e5c7b7263241298aeb664a178267d9e0af12c17b
manjit5190 <manjit5190@gmail.com>
2018-06-22 01:32:52 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const Path = require('path');
+const Pkg = require(Path.join(__dirname, '..', 'package.json'));
+const express = require('express');
+
+const app = express();
+
+// Register middleware that parses the request payload.
+app.use(require('body-parser').raw({
+    type: 'application/jwt'
+}));
+
+// Route that is called for every contact who reaches the custom split activity
+app.post('/activity/execute', (req, res) => {
+    verifyToken(req.body, Pkg.options.salesforce.marketingCloud.jwtSecret, (err, decoded) => {
+        // verification error -> unauthorized request
+        if (err) {
+            console.error(err);
+            return res.status(401).end();
+        }
+
+        if (parseInt(Math.random() * 100) % 2 === 0) {
+            return res.status(200).json({branchResult: 'is_working'});
+        } else {
+            return res.status(200).json({branchResult: 'is_holiday'});
+        }
+    });
+});
+
+// Serve the custom activity's interface, config, etc.
+app.use(express.static(Path.join(__dirname, '..', 'public')));
+
+// Start the server and listen on the port specified by heroku or defaulting to 12345
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Service Cloud custom split backend is now running!');
+});
+
+
+function verifyToken(body, secret, cb) {
+    if (!body) {
+        return cb(new Error('invalid jwtdata'));
+    }
+
+    require('jsonwebtoken').verify(body.toString('utf8'), secret, {
+        algorithm: 'HS256'
+    }, cb);
+}
+
+
---
+++ public/customActivity.js
@@ -0,0 +1,78 @@
+'use strict';
+
+define(function (require) {
+    let Postmonger = require('postmonger');
+    let connection = new Postmonger.Session();
+    let payload = {};
+    let steps = [
+        {'key': 'message_only', 'label': 'Holiday Check'}
+    ];
+    let currentStep = steps[0].key;
+    let eventDefinitionKey = '';
+    let deFields = [];
+
+    $(window).ready(function () {
+        connection.trigger('ready');
+        connection.trigger('requestInteraction');
+    });
+
+    function initialize(data) {
+        if (data) {
+            payload = data;
+        }
+    }
+
+    function onClickedNext() {
+        if (currentStep.key === 'idselection') {
+            save();
+        } else {
+            connection.trigger('nextStep');
+        }
+    }
+
+    function onClickedBack() {
+        connection.trigger('prevStep');
+    }
+
+    function onGotoStep(step) {
+        showStep(step);
+        connection.trigger('ready');
+    }
+
+    function showStep(step, stepIndex) {
+        if (stepIndex && !step) {
+            step = steps[stepIndex - 1];
+        }
+
+        currentStep = step;
+
+        $('.step').hide();
+
+        switch (currentStep.key) {
+            case 'eventdefinitionkey':
+                $('#step1').show();
+                $('#step1 input').focus();
+                break;
+            case 'idselection':
+                $('#step2').show();
+                $('#step2 input').focus();
+                break;
+        }
+    }
+
+    function save() {
+        payload['arguments'] = payload['arguments'] || {};
+        payload['arguments'].execute = payload['arguments'].execute || {};
+        payload['metaData'] = payload['metaData'] || {};
+        payload['metaData'].isConfigured = true;
+
+        console.log(JSON.stringify(payload));
+
+        connection.trigger('updateActivity', payload);
+    }
+
+    connection.on('initActivity', initialize);
+    connection.on('clickedNext', onClickedNext);
+    connection.on('clickedBack', onClickedBack);
+    connection.on('gotoStep', onGotoStep);
+});
