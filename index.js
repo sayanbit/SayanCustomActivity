647a91fca1b945ed4deb42c91f490a31e436def9
manjit5190 <manjit5190@gmail.com>
2018-07-05 02:08:32 +0530

    TAG - Updated Logic to fetch holidays

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ const app = express();
 app.use(require('body-parser').raw({
     type: 'application/jwt'
 }));
+//app.use(require('body-parser').json());
 
 // Route that is called for every contact who reaches the custom split activity
 app.post('/activity/execute', (req, res) => {
     verifyToken(req.body, Pkg.options.salesforce.marketingCloud.jwtSecret, (err, decoded) => {
-        console.log('REQUEST RECEIVED');
-        console.log(JSON.stringify(decoded));
+        console.log('REQUEST RECEIVED', JSON.stringify(decoded));
         // verification error -> unauthorized request
         if (err) {
-            console.error(err);
+            console.error('ERROR VERIFICATION: ', err);
             return res.status(401).end();
         }
 
@@ -36,16 +36,7 @@ app.post('/activity/execute', (req, res) => {
         if (!dataExtensionName || !fieldToUpdate || !daysToSendEmailOn || !subKey) {
             return res.status(400).end();
         } else {
-            let updateResponse = sfmc.updateDataExtension(dataExtensionName, fieldToUpdate, subKey, holidayDataExtensionName, daysToSendEmailOn);
-            console.log('----------------------RESPONSE--------------------------');
-            console.log(updateResponse.statusCode);
-            if (updateResponse.statusCode === 200) {
-                let parsedResponse = JSON.parse(updateResponse.getBody('utf8'));
-                console.log('parsedResponse', updateResponse.getBody('utf8'));
-                return res.status(200).json({branchResult: 'forward'});
-            } else {
-                return res.status(400).end();
-            }
+            sfmc.updateDataExtension(dataExtensionName, fieldToUpdate, subKey, holidayDataExtensionName, daysToSendEmailOn, res);
         }
     });
 });
--- sfmc-update.js
+++ sfmc-update.js
@@ -2,49 +2,19 @@ const BASE_URL = 'https://www.exacttargetapis.com';
 let UPDATE_URL = '/hub/v1/dataevents/key:DE_ID/rows/SubscriberKey:SUB_KEY';
 
 let request = require('sync-request');
+let FuelSDK = require('fuelsdk-node');
 const Path = require('path');
+const moment = require('moment');
 const Pkg = require(Path.join(__dirname, 'package.json'));
 
-let authResponse = '';
-
-
-module.exports.updateDataExtension = function (dataExtensionKey, fieldToUpdate, subKey, holidayDataExtensionName, daysToSendEmailOn) {
-    let jsonBody = {
-        "values": {}
-    };
-    let accessToken = getAccessToken();
-    jsonBody.values[fieldToUpdate] = checkBusinessDay(holidayDataExtensionName, daysToSendEmailOn);
-    console.log('BODY: ',
-        JSON.stringify(jsonBody),
-        BASE_URL + UPDATE_URL.replace(/DE_ID/g, dataExtensionKey).replace(/SUB_KEY/g, subKey),
-        "Bearer " + accessToken);
-    let res;
-    res = request('PUT', BASE_URL + UPDATE_URL.replace(/DE_ID/g, dataExtensionKey).replace(/SUB_KEY/g, subKey),
-        {
-            json: jsonBody,
-            headers: {
-                "Authorization": "Bearer " + accessToken
-            }
-        }
-    );
-    return res;
-
-};
-
-function checkBusinessDay(holidayDataExtensionName, daysToSendEmailOn) {
-    const daysArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let thisDate = new Date();
-    let currentDay = daysArray[thisDate.getDay()];
-    while (daysToSendEmailOn.indexOf(currentDay) === -1) {
-        thisDate.setHours(thisDate.getHours() + 24);
-        currentDay = daysArray[thisDate.getDay()];
-    }
-    return thisDate.toISOString();
-    //let dateArray = [thisDate.getFullYear(), thisDate.getMonth() + 1, thisDate.getDate()];
-    //return dateArray.join('-');
-}
+const loginOptions = Pkg.options.salesforce.marketingCloud;
+const ET_Client = new FuelSDK(loginOptions.clientId, loginOptions.clientSecret, loginOptions.marketingCloudInstance);
+const defaultDateFormat = loginOptions.defaultDateFormat;
+let accessToken = null;
+let holidayListCache = {};
 
 function getAccessToken() {
+    if (accessToken) return accessToken;
     let response = request('POST', 'https://auth.exacttargetapis.com/v1/requestToken',
         {
             json: {
@@ -55,6 +25,115 @@ function getAccessToken() {
             }
         }
     );
-    console.log(response.getBody("utf8"));
-    return JSON.parse(response.getBody("utf8")).accessToken;
+    console.log('TOKEN: ', response.getBody("utf8"));
+    accessToken = JSON.parse(response.getBody("utf8")).accessToken;
+    return accessToken;
 }
+
+function parseHolidayList(result, fieldName, format) {
+    let holidayList = [];
+
+    fieldName = fieldName || 'Holiday Date';
+    format = format || defaultDateFormat;
+
+    if (result.body && result.body.Results) {
+        result.body.Results.forEach(function (value) {
+            value.Properties.Property.forEach(function (innerValue) {
+                if (innerValue.Name.toLowerCase() === fieldName.toLowerCase()) {
+                    holidayList.push(moment.utc(innerValue.Value, format).format(defaultDateFormat));
+                }
+            })
+        })
+    }
+    let todayDateUTC = moment.utc().format(defaultDateFormat);
+    //TO clear all previous values
+    holidayListCache = [];
+    //Cache the holiday response so that we do not have to make call out again and again.
+    holidayListCache[todayDateUTC] = holidayList;
+
+    return holidayList;
+}
+
+module.exports.updateDataExtension = function (dataExtensionKey, fieldToUpdate, subKey, holidayDataExtensionName, daysToSendEmailOn, res) {
+    // Only get holidays starting today. Default 2500 holidays will be fetched.
+    let datTodayDate = moment.utc().subtract('1000', 'days').format(defaultDateFormat);
+    holidayDataExtensionName = holidayDataExtensionName || 'HolidayList';
+
+    let options = {
+        Name: "SDKDataExtension"
+        , props: ['Holiday Date', 'Country']
+        , filter: {
+            leftOperand: 'Holiday Date',
+            operator: 'greaterThan',
+            rightOperand: datTodayDate
+        }
+
+    };
+
+    let deRow = ET_Client.dataExtensionColumn(options);
+    deRow.objName = `DataExtensionObject[${holidayDataExtensionName}]`; //Default: HolidayList
+    let deRowRead = new Promise(function (resolve, reject) {
+        let todayDateUTC = moment.utc().format(defaultDateFormat);
+        if (holidayListCache[todayDateUTC]) {
+            resolve(holidayListCache[todayDateUTC])
+        }
+        else {
+            deRow.get(function (err, response) {
+                if (err) {
+                    reject(err);
+                } else {
+                    let parsedHolidays = parseHolidayList(response);
+                    resolve(parsedHolidays);
+                }
+            })
+        }
+    });
+
+    deRowRead.then(function (holidayListResponse) {
+        let thisDate = moment.utc();
+        let isHoliday = true;
+        let currentDay = thisDate.format('dddd');
+
+        console.log(thisDate.format(defaultDateFormat));
+
+        console.log('sfmc-update', 'Holiday List', JSON.stringify(holidayListResponse));
+
+        while (daysToSendEmailOn.indexOf(currentDay) === -1 || isHoliday === true) {
+
+            thisDate = thisDate.add('1', 'days');
+            currentDay = thisDate.format('dddd');
+            console.log(currentDay, thisDate.format(defaultDateFormat), isHoliday);
+            if (holidayListResponse.length > 0) {
+                isHoliday = holidayListResponse.includes(thisDate.format(defaultDateFormat));
+                console.log(isHoliday);
+            }
+        }
+
+        console.log('sfmc-update', 'Holiday List', thisDate.format(defaultDateFormat));
+
+        let jsonBody = {
+            "values": {}
+        };
+        jsonBody.values[fieldToUpdate] = thisDate.format(defaultDateFormat);
+
+        let updateResponse = request('PUT', BASE_URL + UPDATE_URL.replace(/DE_ID/g, dataExtensionKey).replace(/SUB_KEY/g, subKey),
+            {
+                json: jsonBody,
+                headers: {
+                    "Authorization": "Bearer " + getAccessToken()
+                }
+            }
+        );
+        if (updateResponse.statusCode === 200) {
+            let parsedResponse = JSON.parse(updateResponse.getBody('utf8'));
+            console.log('parsedResponse', updateResponse.getBody('utf8'));
+            return res.status(200).json({branchResult: 'forward'});
+        } else {
+            return res.status(400).end();
+        }
+    }).catch(reason => {
+        console.log('Error in sfmc-update.js: ', reason);
+        return res.status(400).end();
+    });
+
+};
